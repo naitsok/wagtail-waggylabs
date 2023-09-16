2451aa385dcd265de6a2e263567daa550dcea14e
Konstantin <contact@ktamarov.com>
2023-09-16 21:50:54 +0300

    Testing GitHub actions.

--- compose/prod/web/start.sh
+++ compose/prod/web/start.sh
@@ -5,7 +5,8 @@ set -o pipefail
 set -o nounset
 
 python manage.py migrate --noinput
-python manage.py collectstatic
+python manage.py collectstatic --clear
+python manage.py collectstatic --noinput
 python manage.py createsuperuser_container
 # python manage.py runserver 0.0.0.0:8000
 gunicorn waggylabs_site.wsgi:application --bind 0.0.0.0:8000
