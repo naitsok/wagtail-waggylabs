2590cf3d0334ba4012e7cd63f462015970e20bc7
Konstantin <contact@ktamarov.com>
2025-10-20 21:10:23 +0300

    Wagtail 1.2.0 Dockerfile fix.

--- compose/prod/web/start.sh
+++ compose/prod/web/start.sh
@@ -6,7 +6,9 @@ set -o nounset
 
 python manage.py migrate --noinput
 # python manage.py collectstatic --clear --noinput
-python manage.py collectstatic --noinput
+# moved collectstatic call to docker file because here
+# it causes permission denied error
+# python manage.py collectstatic --noinput
 python manage.py createsuperuser_container
 # python manage.py runserver 0.0.0.0:8000
 gunicorn waggylabs_site.wsgi:application --bind 0.0.0.0:8000
