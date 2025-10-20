bf27059872594a481e0ef9495e1b4bc44473c641
Konstantin <contact@ktamarov.com>
2025-10-20 22:23:15 +0300

    WaggyLabs 1.2.0 collectstatic still causes permission denied error on container start.

--- compose/prod/web/start.sh
+++ compose/prod/web/start.sh
@@ -8,8 +8,8 @@ python manage.py migrate --noinput
 # python manage.py collectstatic --clear --noinput
 # moved collectstatic call to docker file because here
 # it causes permission denied error
-chown -R app:app /home/app/web/staticfiles
-python manage.py collectstatic --noinput
+# chown -R app:app /home/app/web/staticfiles
+# python manage.py collectstatic --noinput
 python manage.py createsuperuser_container
 # python manage.py runserver 0.0.0.0:8000
 gunicorn waggylabs_site.wsgi:application --bind 0.0.0.0:8000
