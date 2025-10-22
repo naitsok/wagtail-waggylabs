44de2031f04e1f8cd1a5613ce21f0df67ee2bb5c
Konstantin <contact@ktamarov.com>
2025-10-22 18:57:44 +0300

    WaggyLabs 1.2.0 permission on static volume fixed.

--- compose/prod/web/start.sh
+++ compose/prod/web/start.sh
@@ -6,10 +6,8 @@ set -o nounset
 
 python manage.py migrate --noinput
 # python manage.py collectstatic --clear --noinput
-# moved collectstatic call to docker file because here
-# it causes permission denied error
-# chown -R app:app /home/app/web/staticfiles
-# python manage.py collectstatic --noinput
+# permissions for static and media files fixed
+python manage.py collectstatic --noinput
 python manage.py createsuperuser_container
 # python manage.py runserver 0.0.0.0:8000
 gunicorn waggylabs_site.wsgi:application --bind 0.0.0.0:8000
